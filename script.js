dfc95ea389e67d47347b7b36a6bbd5fe5de310a3
Jivitesh Goyal <jiviteshgoyal1412@gmail.com>
2026-07-13 17:01:08 +0530

    Refactor CSS and organize project assets

--- script.js
+++ script.js
@@ -1,18 +1,129 @@
+// ===============================
+// HERO SLIDER
+// ===============================
+
+const heroSlider = document.getElementById("hero-slider");
+const heroSlides = document.querySelectorAll(".hero-slide");
+const heroLeft = document.getElementById("hero-left");
+const heroRight = document.getElementById("hero-right");
+
+let currentHero = 1;
+const totalSlides = heroSlides.length;
+
+function updateHero() {
+
+    heroSlider.style.transform =
+        `translateX(-${currentHero * 100}%)`;
+
+}
+function nextHero() {
+
+    if (currentHero >= totalSlides - 1) return;
+
+    currentHero++;
+
+    updateHero();
+
+}
+
+function previousHero() {
+
+    if (currentHero <= 0) return;
+
+    currentHero--;
+
+    updateHero();
+
+}
+
+// ---------- Auto Slider ----------
+heroSlider.addEventListener("transitionend", () => {
+
+    // If we reached the first clone
+    if (heroSlides[currentHero].id === "first-clone") {
+
+        heroSlider.style.transition = "none";
+
+        currentHero = 1;
+
+        updateHero();
+
+        requestAnimationFrame(() => {
+            heroSlider.style.transition = "transform .45s ease";
+        });
+
+    }
+
+    // If we reached the last clone
+    if (heroSlides[currentHero].id === "last-clone") {
+
+        heroSlider.style.transition = "none";
+
+        currentHero = totalSlides - 2;
+
+        updateHero();
+
+        requestAnimationFrame(() => {
+            heroSlider.style.transition = "transform .45s ease";
+        });
+
+    }
+
+});
+let heroTimer;
+
+function startHeroTimer() {
+    heroTimer = setInterval(nextHero, 5000);
+}
+
+function resetHeroTimer() {
+    clearInterval(heroTimer);
+    startHeroTimer();
+}
+
+startHeroTimer();
+updateHero();
+
+// ---------- Arrow Buttons ----------
+
+heroRight.addEventListener("click", () => {
+    nextHero();
+    resetHeroTimer();
+});
+
+heroLeft.addEventListener("click", () => {
+    previousHero();
+    resetHeroTimer();
+});
+
+// ===============================
+// PRODUCT SLIDERS
+// ===============================
+
 const productSliders = document.querySelectorAll(".product-slider");
+
 productSliders.forEach((sliderSection) => {
+
     const slider = sliderSection.querySelector(".slider-track");
     const leftBtn = sliderSection.querySelector(".slider-left");
     const rightBtn = sliderSection.querySelector(".slider-right");
+
     rightBtn.addEventListener("click", () => {
+
         slider.scrollBy({
             left: 500,
             behavior: "smooth"
         });
+
     });
+
     leftBtn.addEventListener("click", () => {
+
         slider.scrollBy({
             left: -500,
             behavior: "smooth"
         });
+
     });
+
 });
